98c6df7eaf9556e1e8ad8864bd916adf44aff796
J-BONNEY <bonfacemutui44@gmail.com>
2026-08-18 20:19:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# It has to have Routes: e.g., /login, /register, /, /dashboard?query=1.
+# it has to have a method eg GET,POST,PUT,DELETE,OPTIONS,PATCH.
+# It has to have a status code e.g 200,201,403,405
+# it has to have transfer data in JSON (Key:value pairs).
+from flask import Flask,request,jsonify
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    if request.method=="GET":
+        data={"Flask API" : "Version 1"}
+        return jsonify(data),200
+    else:
+        error={"error" : "Method not allowed"}
+        return jsonify(error),403
+    

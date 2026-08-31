b40075758185f0a1a0f848c643ec60554636f9b3
J-BONNEY <bonfacemutui44@gmail.com>
2026-08-31 19:26:26 +0300

    Initial commit

--- main.py
+++ main.py
@@ -2,17 +2,215 @@
 # it has to have a method eg GET,POST,PUT,DELETE,OPTIONS,PATCH.
 # It has to have a status code e.g 200,201,403,405
 # it has to have transfer data in JSON (Key:value pairs).
-from flask import Flask,request,jsonify
+from datetime import datetime
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from models import Base, Product, User, Purchase,Sale,Sale_detail,Payment
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+# create a connection to the database using sqlalchemy
+engine = create_engine("sqlite:///./flask_duka_api.db", echo=True)
+
+# create tables into the database using sqlalchemy
+Base.metadata.create_all(engine)
+
+# create a session to do sql transactions
+session = Session(engine)
+
+user = {"id": "1",
+        "full_name": "Bonney",
+        "email": "bon@gmail.com",
+        "password": "bonn0",
+        }
+
+
+@app.before_request
+def before_request():
+    try:
+        print("A request is coming in")
+        new_user = User(user)
+        session.add(new_user)
+        session.commit()
+        return jsonify({"message": "User added successfully}"}), 201
+    except:
+        print("Error found")
+
 
 @app.route("/")
 def home():
-    if request.method=="GET":
-        data={"Flask API" : "Version 1"}
-        return jsonify(data),200
+    if request.method == "GET":
+        data = {"Flask API": "Version 1"}
+        return jsonify(data), 200
+    else:
+        error = {"error": "Method not allowed"}
+        return jsonify(error), 405
+
+# PRODUCTS ROUTE.
+@app.route("/products", methods=["GET", "POST"])
+def products():
+    if request.method == "GET":
+        # fetch data from the database.
+        query = select(Product)
+        products = session.scalars(query)
+        results = []
+        for prod in products:
+            p = {"id": prod.id, "product_name": prod.product_name,
+                 "buying_price": prod.buying_price, 
+                 "selling_price": prod.selling_price}
+            results.append(p)
+        return jsonify(results), 200
+
+    elif request.method == "POST":
+        data = request.get_json()
+        if data["product_name"] == "" or data["buying_price"] == "" or data["selling_price"] == "":
+            error = {"error": "Ensure all fields are set"}
+            return jsonify(error), 403
+        else:
+            # store in the database
+            new_product = Product(
+                user_id=user["id"],
+                product_name=data["product_name"],
+                buying_price=float(data["buying_price"]),
+                selling_price=float(data["selling_price"])
+            )
+            session.add(new_product)
+            session.commit()
+        return jsonify({"message": "A new product added successfully"}), 201
+    else:
+        error = {"error": "method not allowed"}
+        return jsonify(error), 405
+
+# PURCHASE ROUTE.
+@app.route("/purchases", methods=["GET", "POST"])
+def purchases():
+    if request.method =="GET":
+        # FETCH DATA FROM DATABASE.
+        query= select (Purchase)
+        purchases = session.scalars(query)
+        results =[]
+        for purch in purchases:
+            pu= {"product_id" : purch.id,"quantity" : purch.quantity,"paid_amount" :purch.paid_amount,
+                 "created_at": purch.created_at}
+            results.append(pu)
+        return jsonify(results),200
+    elif request.method == "POST":
+        data = request.get_json()
+        if data ["quantity"] == "" or data ["paid_amount"] == "" or data["created_at"]=="":
+            error={"error": "Ensure all fields are set"}
+            return jsonify(error),403
+        else:
+            # STORE IN THE DATABASE.
+            new_purchase=Purchase(
+                product_id= data["product_id"],
+                quantity= data["quantity"],
+                paid_amount= data["paid_amount"],
+                created_at =datetime.now()
+            )
+            session.add(new_purchase)
+            session.commit()
+        return jsonify ({"Message" :"A new purchase made successfully"}), 201
+    else:
+        error = {"error" : "Method not allowed"}
+        return jsonify(error),405
+
+# SALE ROUTE
+@app.route("/sales", methods=["GET","POST"])
+def sales():
+    if request.method =="GET":
+        # FETCH DATA FROM DATABASE.
+        query= select (Sale)
+        sales=session.scalars(query)
+        results =[]
+        for sal in sales:
+            s={"id" :sal.id, "product_id" : sal.product_id,"created_at": sal.created_at}
+            results.append(s)
+        return jsonify(results),200
+    elif request.method== "POST":
+         data =request.get_json()
+         if data ["product_id"] =="":
+             error={"error" : "Ensure all the fields are set"}
+             return jsonify(error),403
+         else:
+            #  STORE IN THE DATABASE.
+            new_sale=Sale(
+                product_id= data["product_id"],
+                created_at =datetime.now()
+            )
+            session.add(new_sale)
+            session.commit()
+         return jsonify({"Message" : "New sale successfully made"}),201
     else:
         error={"error" : "Method not allowed"}
         return jsonify(error),405
+
+#  SALE_DETAILS.
+@app.route("/sale_details", methods = ["GET","POST"])
+def sales_details():
+          if request.method=="GET":
+            #   FETCH DATA FROM DATABASE.
+            query= select (Sale_detail)
+            sale_details = session.scalars(query)
+            results =[]
+            for sald in sale_details:
+                sd={"id" : sald.id, "product_id" :sald.product_id,"quantity":sald.quantity}
+                results.append(sd)
+            return jsonify(results),200
+          elif request.method=="POST":
+              data=request.get_json()
+              if data ["product_id"] =="" or data ["quantity"] =="":
+                  error={"error","Ensure all fields are set"}
+                  return jsonify(error),403
+              else:
+                #   STORE IN THE DATABASE.
+                new_sale_details=Sale_detail(
+                    product_id=data["product_id"],
+                    sale_id=data["sale_id"],
+                    quantity=data["quantity"]
+                )
+                session.add(new_sale_details)
+                session.commit()
+                return jsonify ({"Message" :"Sale Details successfully added"})
+          else:
+              error={"error" : "Method not allowed"}
+              return jsonify(error),405
+              
+# PAYMENTS.
+@app.route("/payments",methods =["GET","POST"])
+def payments():
+    if request.method=="GET":
+        # FETCH DATA FROM DATABASE.
+        query=select (Payment)
+        payments=session.scalars(query)
+        results=[]
+        for py in payments:
+            py={"id":py.id,"amount":py.amount,"method" :py.method,"transcode" :py.trans_code,
+                "status":py.status,"created_at":py.created_at}
+            results.append(py)
+        return jsonify(results),200
+    elif request.method=="POST":
+        data = request.get_json()
+        if data ["amount"] == "" or data["method"]== "" or data["transcode"]=="" or data["status"]=="":
+           error={"error":"Ensure all fields are set"}
+           return jsonify(error),403
+        else:
+            # STORE IN THE DATABASE.
+            new_payment=Payment(
+                id=data["id"],
+                amount=data["amount"],
+                method= data["method"],
+                trans_code=data["transcode"],
+                status=data["status"],
+                created_at=datetime.now()
+            )
+            session.add(new_payment)
+            session.commit()
+        return jsonify ({"Message":"payment successfully made"}),201
+    else:
+        error={"error" : "Method not allowed"}
+        return jsonify(error),405
+    
+
 
 app.run(debug=True)
--- models.py
+++ models.py
@@ -18,75 +18,55 @@ class User(Base):
 class Product(Base):
        __tablename__="products"
        id:Mapped[int]=mapped_column(Integer,primary_key=True)
-       user_id:Mapped[int]=mapped_column(ForeignKey("user_id"))
+       user_id:Mapped[int]=mapped_column(ForeignKey("users.id"))
+       product_name:Mapped[str]=mapped_column(String(100))
        buying_price:Mapped[float]=mapped_column(Float)
        selling_price:Mapped[float]=mapped_column(Float)
-
 
 class Purchase(Base):
        __tablename__="purchases"
        id:Mapped[int]=mapped_column(Integer,primary_key=True)
-       product_id:Mapped[int]=mapped_column(Integer,ForeignKey("product_id"))
-       supplier_id:Mapped[int]=mapped_column(Integer)
+       product_id:Mapped[int]=mapped_column(Integer,ForeignKey("products.id"))
+       # supplier_id:Mapped[int]=mapped_column(Integer)
        quantity:Mapped[int]=mapped_column(Integer)
-       buying_price:Mapped[float]=mapped_column(Float)
-       total_amount:Mapped[float]=mapped_column(Float)
-       purchase_date:Mapped[DateTime]=mapped_column(DateTime)
+       paid_amount:Mapped[float]=mapped_column(Float)
+       created_at:Mapped[DateTime]=mapped_column(DateTime)
 
 class Sale(Base):
        __tablename__="sales"
        id:Mapped[int]=mapped_column(Integer,primary_key=True)
-       product_id:Mapped[int]=mapped_column(Integer,ForeignKey("product_id"))
-       quantity:Mapped[int]=mapped_column(Integer)
-       selling_date:Mapped[float]=mapped_column(Float)
-       total_amount:Mapped[float]=mapped_column(Float)
-       sale_date:Mapped[DateTime]=mapped_column(DateTime)
+       product_id:Mapped[int]=mapped_column(Integer,ForeignKey("products.id"))
+       created_at:Mapped[DateTime]=mapped_column(DateTime)
 
-
-class Sales_details(Base):
+class Sale_detail(Base):
        __tablename__="sale_details"
        id:Mapped[int]=mapped_column(Integer,primary_key=True)
-       product_id:Mapped[int]=mapped_column(Integer,ForeignKey("product_id"))
-       sale_id:Mapped[int]=mapped_column(Integer,ForeignKey("sale_id"))
+       product_id:Mapped[int]=mapped_column(Integer,ForeignKey("products.id"))
+       sale_id:Mapped[int]=mapped_column(Integer,ForeignKey("sales.id"))
        quantity:Mapped[int]=mapped_column(Integer)
-       selling_date:Mapped[float]=mapped_column(Float)
-       total_amount:Mapped[float]=mapped_column(Float)
 
-class order(Base):
-    __tablename__ = "order"
+# class Supplier(Base):
+#        __tablename__="suppliers"
+#        id:Mapped[int]=mapped_column(Integer,primary_key=True)
+#        product_id:Mapped[str]=mapped_column(String,ForeignKey("products.id"))
+#        Supplier_name:Mapped[str]=mapped_column(String(100))
+#        phone:Mapped[str] = mapped_column(String(30))
+#        email:Mapped[str]=mapped_column(String(100))
+#        address:Mapped[str]=mapped_column(String)
+#        country:Mapped[str]=mapped_column(String)
 
+# class Stock(Base):
+#        __tablename__="stock"
+#        id:Mapped[str]=mapped_column(String,primary_key=True)
+#        product_id:Mapped[str]=mapped_column(String,ForeignKey("products.id"))
+#        supplier_name:Mapped[str]=mapped_column(String(100),ForeignKey("suppliers.id"))
+#        stock_quantity:Mapped[str]=mapped_column(String)
+
+class Payment(Base):
+    __tablename__ = "payments"
     id: Mapped[str] = mapped_column(String, primary_key=True)
-    customer_id: Mapped[str] = mapped_column(String, ForeignKey("customer.id"))
-    product_id: Mapped[str] = mapped_column(String, ForeignKey("product.id"))
-    quantity: Mapped[int] = mapped_column(Integer)
-    order_status: Mapped[str] = mapped_column(String)
-    order_date: Mapped[DateTime] = mapped_column(DateTime)
-    total_amount: Mapped[float] = mapped_column(Float)
-
-    
-class Supplier(Base):
-       __tablename__="suppliers"
-       id:Mapped[int]=mapped_column(Integer,primary_key=True)
-       product_id:Mapped[str]=mapped_column(String,ForeignKey("product_id"))
-       Supplier_name:Mapped[str]=mapped_column(String(100))
-       phone:Mapped[str] = mapped_column(String(30))
-       email:Mapped[str]=mapped_column(String(100))
-       address:Mapped[str]=mapped_column(String)
-       country:Mapped[str]=mapped_column(String)
-
-class stock(Base):
-       __tablename__="stock"
-       id:Mapped[str]=mapped_column(String,primary_key=True)
-       product_id:Mapped[str]=mapped_column(String,ForeignKey("product_id"))
-       supplier_name:Mapped[str]=mapped_column(String(100),ForeignKey("supplier_name"))
-       stock_quantity:Mapped[str]=mapped_column(String)
-
-class payment(Base):
-    __tablename__ = "payment"
-
-    id: Mapped[str] = mapped_column(String, primary_key=True)
-    order_id: Mapped[str] = mapped_column(String, ForeignKey("order.id"))
     amount: Mapped[float] = mapped_column(Float)
-    payment_method: Mapped[str] = mapped_column(String(70))
-    payment_status: Mapped[str] = mapped_column(String(30))
-    payment_date: Mapped[DateTime] = mapped_column(DateTime)
+    method: Mapped[str] = mapped_column(String(70))
+    trans_code:Mapped[str]=mapped_column(String(200))
+    status: Mapped[str] = mapped_column(String(30))
+    created_at: Mapped[DateTime] = mapped_column(DateTime)
